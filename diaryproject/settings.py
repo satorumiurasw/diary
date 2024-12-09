cb83a95b971e8a8965fd505dd6ce18180b93bf81
Satoru Miura <satoru@miurasw.com>
2024-12-09 13:21:55 +0900

    add STATIC_ROOT to settings.py

--- diaryproject/settings.py
+++ diaryproject/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/' # static_file の読み込み場所を /static/ に指定 (変更なし)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
