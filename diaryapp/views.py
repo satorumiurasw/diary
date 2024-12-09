6d655cef6405bba9ba77e78cde5481482151adbd
Satoru Miura <satoru@miurasw.com>
2024-12-09 11:30:30 +0900

    add CRUD features

--- diaryapp/views.py
+++ diaryapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import DiaryModel
+from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class DiaryList(ListView):
+    template_name = 'diaryapp/list.html'
+    model = DiaryModel
+
+class DiaryCreate(CreateView):
+    template_name = 'diaryapp/create.html'
+    model = DiaryModel
+    fields = ('date', 'title', 'text')
+    success_url = reverse_lazy('list')
+
+class DiaryDetail(DetailView):
+    template_name = 'diaryapp/detail.html'
+    model = DiaryModel
+    
